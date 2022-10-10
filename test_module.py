5c36a6d8289a3594154e13fd14032a0868955c9b
Don Assamongkol <dassamongkol@cs.uchicago.edu>
2022-10-10 15:49:32 -0500

    code is complete, need to run on SLURM

--- test_module.py
+++ test_module.py
@@ -1,6 +1,7 @@
 import subprocess
 import test_generator
 import os
+import filecmp
 
 # run using python3 test_module.py
 
@@ -14,33 +15,68 @@ for N in Ns_to_generate:
 # compile the code
 os.system("make serial parallel")
 
+
 # run the serial program for various inputs
 print("\nrunning serial execution")
-print('N T time')
+Ns = []
+serial_times = []
 for filename in filenames:
     rv = subprocess.run(["./serial_FW", filename, "1"], capture_output=True, text=True)
     N = rv.args[1].split("_")[1]
-    T = rv.args[2] # number of threads (always 1 if serial)
     time = rv.stdout.split(":")[1].strip() # time in ms 
-    print(N, T, time)
+    
+    Ns.append(N)
+    serial_times.append(time)
 
+print("N: ", Ns)
+print("serial_times: ", serial_times)
+
+Ns = [] 
+parallel_overhead_times = [] 
 print("\nrunning parallel overhead execution T=1")
 for filename in filenames:
     rv = subprocess.run(["./parallel_FW", filename, "1"], capture_output=True, text=True)
     N = rv.args[1].split("_")[1]
-    T = rv.args[2] # number of threads (always 1 if serial)
     time = rv.stdout.split(":")[1].strip() # time in ms 
-    
-    print(N, T, time)
 
-# print("\nrunning parallel speedup executions")
-# Ts_to_test = [2, 4, 8, 16, 32, 64]
-# for filename in filenames:
-#     for T in Ts_to_test:
-#         rv = subprocess.run(["./parallel_FW", filename, str(T)], capture_output=True, text=True)
-#         print(rv)
-#     print(" ")
+    Ns.append(N)
+    parallel_overhead_times.append(time)
     
+print("N: ", Ns)
+print("parallel_overhead_times: ", parallel_overhead_times)
+
+print("\nrunning parallel speedup executions")
+Ts_to_test = [2, 4, 8, 16, 32, 64]
+for filename in filenames:
+    thread_times = []
+
+    for T in Ts_to_test:
+        rv = subprocess.run(["./parallel_FW", filename, str(T)], capture_output=True, text=True)
+        N = rv.args[1].split("_")[1]
+        time = rv.stdout.split(":")[1].strip() # time in ms 
+
+        thread_times.append(time)
+
+        if T == 64:
+            print(f"for N={N}", thread_times)
+    
+print("verifying that outputs are the same...")
+files_to_compare = [
+    "tests/N_16_T_1_serial_output.txt",
+    "tests/N_16_T_1_parallel_output.txt", 
+    "tests/N_16_T_2_parallel_output.txt", 
+    "tests/N_16_T_4_parallel_output.txt", 
+    "tests/N_16_T_8_parallel_output.txt", 
+    "tests/N_16_T_16_parallel_output.txt", 
+    "tests/N_16_T_32_parallel_output.txt", 
+    "tests/N_16_T_64_parallel_output.txt", 
+]
+for file in files_to_compare:
+    if not filecmp.cmp(files_to_compare[0], file, shallow=False):
+        print("Error! Did not correctly generate dist array between serial/parallel versions of our code!")
+
+print("Done!")
+
 
 # input = 
 # want to then process this into two graphs: 
